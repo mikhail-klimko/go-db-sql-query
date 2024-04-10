f7be3224df69ba750772a190888e3158e4592ffc
mikhail-klimko <mklimko123@gmail.com>
2024-04-10 13:22:23 +0300

    wip: Wed Apr 10 13:22:23 +03 2024

--- main.go
+++ main.go
@@ -85,17 +85,35 @@ func main() {
 
 func insertClient(db *sql.DB, client Client) (int64, error) {
 	// напишите здесь код для добавления новой записи в таблицу clients
+	res, err := db.Exec("INSERT INTO clients (fio, login, birthday, email) VALUES (:fio, :login, :birthday, :email)",
+		sql.Named("fio", client.FIO), sql.Named("login", client.Login), sql.Named("birthday", client.Birthday), sql.Named("email", client.Email))
 
-	return 0, nil // вместо 0 верните идентификатор добавленной записи
+	if err != nil {
+		return 0, err
+	}
+
+	return res.LastInsertId() // вместо 0 верните идентификатор добавленной записи
 }
 
 func updateClientLogin(db *sql.DB, login string, id int64) error {
 	// напишите здесь код для обновления поля login в таблице clients у записи с заданным id
+	_, err := db.Exec("UPDATE clients SET login = :login WHERE id = :id", sql.Named("login", login), sql.Named("id", id))
+
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
 func deleteClient(db *sql.DB, id int64) error {
 	// напишите здесь код для удаления записи из таблицы clients по заданному id
+	_, err := db.Exec("DELETE FROM clients WHERE id = :id", sql.Named("id", id))
+
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
